616bcec117552c3cb4999da586d926814f46265d
Nikita Feldbush <fn11100@gmail.com>
2022-01-14 16:15:17 +0700

    New one 2

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<title>Карта</title>
+
+</head>
+<body>
+    
+</body>
+</html>
 <?php
-    header ("Content-type: image/png");
 	$ip = "localhost";
 	$user = "u1175838_root";
 	$password = "nik010802_";
@@ -16,28 +27,10 @@
 		die("Ошибка в соединении: " .$mysql->connect_error);
 	}
 
-
-	$sql = mysqli_query($mysql, 'SELECT `r`, `g`, `b` FROM `colors_1`');
-	$im = imagecreatetruecolor(1920,1080);
-    $white = imagecolorallocate($im, 255, 255, 255);
-    imageFilledRectangle($im, 0, 0, 1920, 1080, $white);
-
-
+    mysqli_set_charset($mysql, "utf8");
+    $sql = mysqli_query($mysql, 'SELECT DISTINCT `global_id`, `District` FROM `Data`');
+    echo"<select size='1'>";
     while ($result = mysqli_fetch_array($sql)) {
-        if($k<5){
-        imageFilledRectangle($im,$x1,$y1,$x2,$y2,(imagecolorallocate($im,"{$result['r']}", "{$result['g']}", "{$result['b']}")));//
-        $x1=$x1+40;
-        $x2=$x2+40;
-        $k++;
-        }
-        else{
-            $k=0;
-            $y1=$y1+40;
-            $y2=$y2+40;
-            $x1=10;
-            $x2=40;
-        }
+        echo"<option value={$result['global_id']}>{$result['District']}<option>";
     }
-    imagepng($im);
-    imagedestroy($im);
 ?>
