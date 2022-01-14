79a7deb1b67695a9577cd1fde9d10c8009820269
Nikita Feldbush <fn11100@gmail.com>
2022-01-14 15:35:31 +0700

    new commit

--- new.php
+++ new.php
@@ -1,5 +1,5 @@
 <?php
-    header ("Content-type: image/png");
+   // header ("Content-type: image/png");
 	$ip = "localhost";
 	$user = "u1175838_root";
 	$password = "nik010802_";
@@ -21,9 +21,13 @@
 	$im = imagecreatetruecolor(1920,1080);
     $white = imagecolorallocate($im, 255, 255, 255);
     imageFilledRectangle($im, 0, 0, 1920, 1080, $white);
-
+    $array = array(
+    );
 
     while ($result = mysqli_fetch_array($sql)) {
+     
+        array_push($array,"{$result['r']}", "{$result['g']}", "{$result['b']}");
+       
         if($k<5){
         imageFilledRectangle($im,$x1,$y1,$x2,$y2,(imagecolorallocate($im,"{$result['r']}", "{$result['g']}", "{$result['b']}")));//
         $x1=$x1+40;
@@ -40,4 +44,5 @@
     }
     imagepng($im);
     imagedestroy($im);
+    var_dump($array);
 ?>
