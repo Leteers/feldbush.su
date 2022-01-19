fe23092dd257ee68e2630bf5c9cd450032f68f4e
Nikita Feldbush <fn11100@gmail.com>
2022-01-19 18:26:31 +0700

    19.01

--- index.php
+++ index.php
@@ -1,36 +1,33 @@
 <!DOCTYPE html>
 <html>
+        
 <head>
-
+    <?php echo"<link rel='stylesheet' href='style/style7.css'>"; ?>
 	<meta charset="utf-8">
-	<title>Карта</title>
-
+	<title>Елочные базары Москвы</title>
+	<link rel="shortcut icon" href="images/favicon.ico">
+  
 </head>
 <body>
-    
+    <div id=content>
+    	<p class=list> <?php
+	    $ip = "localhost";
+	    $user = "u1175838_root";
+	    $password = "nik010802_";
+    	$table= "u1175838_default";
+
+	    $mysql = mysqli_connect($ip, $user, $password, $table);
+	    if($mysql->connect_error){
+		    die("Ошибка в соединении: " .$mysql->connect_error);
+	    }
+        mysqli_set_charset($mysql, "utf8");
+        $sql = mysqli_query($mysql, 'SELECT DISTINCT `District` FROM `Data` ORDER BY `District`');
+        echo"<select size='1'>";
+        while ($result = mysqli_fetch_array($sql)) {
+            echo"<option value = {$result['District']}>{$result['District']}<option>";
+    }
+        ?>
+ </p>
+	</div>
 </body>
 </html>
-<?php
-	$ip = "localhost";
-	$user = "u1175838_root";
-	$password = "nik010802_";
-	$table= "u1175838_default";
-
-    $x1=10;
-    $x2=40;
-    $y1=10;
-    $y2=40;
-    $k=0;
-
-	$mysql = mysqli_connect($ip, $user, $password, $table);
-	if($mysql->connect_error){
-		die("Ошибка в соединении: " .$mysql->connect_error);
-	}
-
-    mysqli_set_charset($mysql, "utf8");
-    $sql = mysqli_query($mysql, 'SELECT DISTINCT `global_id`, `District` FROM `Data`');
-    echo"<select size='1'>";
-    while ($result = mysqli_fetch_array($sql)) {
-        echo"<option value={$result['global_id']}>{$result['District']}<option>";
-    }
-?>
