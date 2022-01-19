d1f5d32f76d9366bfa731468036d544449b584a2
Nikita Feldbush <fn11100@gmail.com>
2022-01-20 00:44:31 +0700

    20.01

--- index.php
+++ index.php
@@ -1,33 +1,30 @@
 <!DOCTYPE html>
 <html>
-        
 <head>
-    <?php echo"<link rel='stylesheet' href='style/style7.css'>"; ?>
+    <script src="jquery.js"></script>
 	<meta charset="utf-8">
 	<title>Елочные базары Москвы</title>
 	<link rel="shortcut icon" href="images/favicon.ico">
-  
 </head>
 <body>
-    <div id=content>
-    	<p class=list> <?php
+     <button class='b-1'>from input</button>
+     <div class='out-block out-1'></div>
+    <?php
 	    $ip = "localhost";
 	    $user = "u1175838_root";
 	    $password = "nik010802_";
     	$table= "u1175838_default";
-
 	    $mysql = mysqli_connect($ip, $user, $password, $table);
 	    if($mysql->connect_error){
 		    die("Ошибка в соединении: " .$mysql->connect_error);
 	    }
         mysqli_set_charset($mysql, "utf8");
-        $sql = mysqli_query($mysql, 'SELECT DISTINCT `District` FROM `Data` ORDER BY `District`');
-        echo"<select size='1'>";
+        $sql = mysqli_query($mysql, 'SELECT count(*) as CNT, global_id, District FROM `Data` GROUP BY District HAVING count(*)>=1');
+        echo"<select size='1' class='s-1'>";
         while ($result = mysqli_fetch_array($sql)) {
-            echo"<option value = {$result['District']}>{$result['District']}<option>";
-    }
-        ?>
- </p>
-	</div>
-</body>
+            echo"<option value = {$result['global_id']}>{$result['District']}<option>";
+    	}
+    ?>
+	    <script src =script1.js></script>
+	   </body>
 </html>
